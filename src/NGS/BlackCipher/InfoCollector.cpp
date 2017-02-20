39f13fe94f9f480f49479accb4725a7ecbcb5cd9
Five <i.f-ve@ya.ru>
2017-02-20 12:49:31 -0500

    Update InfoCollector.cpp

--- src/NGS/BlackCipher/InfoCollector.cpp
+++ src/NGS/BlackCipher/InfoCollector.cpp
@@ -6,6 +6,26 @@
  */
 
 
+
+/**
+ * 
+ */
+int sub_64FE50()
+{
+  char v1; // [sp+0h] [bp-24h]@1
+  int v2; // [sp+20h] [bp-4h]@1
+
+  sub_4066F0((int)&v1, &off_69200C);
+  v2 = 0;
+  dword_6FDCA4 = 0;
+  dword_6FDCA8 = 0;
+  sub_406420(0, 0);
+  sub_405FE0((int)dword_6FDC94, (int)&v1, 0, 0xFFFFFFFF);
+  v2 = -1;
+  sub_406420(1, 0);
+  return atexit(sub_6512A0);
+}
+
 /**
  * 
  */
