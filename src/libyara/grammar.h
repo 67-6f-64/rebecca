5e118d6fc1b1fcf9159b550ee2b906a061c23cd9
Five <i.f-ve@ya.ru>
2017-02-20 18:07:24 -0500

    Create grammar.h

---
+++ src/libyara/grammar.h
@@ -0,0 +1,32 @@
+/**
+ * grammar.h
+ * https://github.com/VirusTotal/yara/blob/master/libyara/grammar.h
+ *  
+ * @author five (i.f-ve@ya.ru)
+ * @verison (2/20/2017)
+ */
+  
+  
+
+/**
+ * enum yytokentype
+ */
+006597A8 off_6597A8      dd offset aEnd          ; DATA XREF: sub_4F1120+386r
+___:006597A8                                         ; sub_4F1120+4EFr ...
+___:006597A8                                         ; "$end"
+___:006597AC                 dd offset aError        ; "error"
+___:006597B0                 dd offset aUndefined_0  ; "$undefined"
+___:006597B4                 dd offset a_rule_       ; "_RULE_"
+___:006597B8                 dd offset a_private_    ; "_PRIVATE_"
+___:006597BC                 dd offset a_global_     ; "_GLOBAL_"
+___:006597C0                 dd offset a_meta_       ; "_META_"
+___:006597C4                 dd offset a_strings_    ; "_STRINGS_"
+___:006597C8                 dd offset a_condition_  ; "_CONDITION_"
+___:006597CC                 dd offset a_identifier_ ; "_IDENTIFIER_"
+___:006597D0                 dd offset a_string_identi ; "_STRING_IDENTIFIER_"
+___:006597D4                 dd offset a_string_count_ ; "_STRING_COUNT_"
+___:006597D8                 dd offset a_string_offset ; "_STRING_OFFSET_"
+___:006597DC                 dd offset a_string_iden_0 ; "_STRING_IDENTIFIER_WITH_WILDCARD_"
+___:006597E0                 dd offset a_number_     ; "_NUMBER_"
+___:006597E4                 dd offset a_double_     ; "_DOUBLE_"
+...
