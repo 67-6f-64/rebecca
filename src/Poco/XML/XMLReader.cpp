86555156bd33dc2991a53d944ed59cef1eb01a67
Five <i.f-ve@ya.ru>
2017-02-20 19:30:16 -0500

    Update XMLReader.cpp

--- src/Poco/XML/XMLReader.cpp
+++ src/Poco/XML/XMLReader.cpp
@@ -1,3 +1,20 @@
+/** 
+ *   This file is part of rebecca.
+ *
+ *   rebecca is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   rebecca is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with rebecca.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * XMLReader.cpp
  * poco/XML/src/XMLReader.cpp
