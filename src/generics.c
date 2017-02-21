a4d2de73e2d531c1b8a0c27d6ddbf070a4da3d99
Five <i.f-ve@ya.ru>
2017-02-21 09:22:58 -0500

    Update generics.c

--- src/generics.c
+++ src/generics.c
@@ -17,64 +17,19 @@
 
 
 /**
- * 
+ * _lock_file
+ *
+ * FILE* file  
  */
-int __thiscall sub_4315A0(int this, size_t a2, char a3)
+void __cdecl sub_5A39F6(unsigned int a1)
 {
-  size_t v4; // [sp+0h] [bp-8h]@4
-  int v5; // [sp+4h] [bp-4h]@1
-
-  v5 = this;
-  if ( -1 - *(_DWORD *)(this + 16) <= a2 )
-    sub_4FC42E("string too long");
-  if ( a2 )
+  if ( a1 < (unsigned int)&off_6E1AE0 || a1 > (unsigned int)dword_6E1D40 )
   {
-    v4 = a2 + *(_DWORD *)(this + 16);
-    if ( (unsigned __int8)sub_4064B0(a2 + *(_DWORD *)(this + 16), 0) )
-    {
-      sub_431660(*(_DWORD *)(v5 + 16), a2, a3);
-      sub_406580(v4);
-    }
+    EnterCriticalSection((LPCRITICAL_SECTION)(a1 + 32));
   }
-  return v5;
-}
-
-/**
- * 
- */
-int __thiscall sub_405FE0(int this, int a2, unsigned int a3, unsigned int a4)
-{
-  int v5; // [sp+18h] [bp-10h]@11
-  void *v6; // [sp+1Ch] [bp-Ch]@14
-  unsigned int v7; // [sp+20h] [bp-8h]@3
-  int v8; // [sp+24h] [bp-4h]@1
-
-  v8 = this;
-  if ( *(_DWORD *)(a2 + 16) < a3 )
-    sub_4FC45C("invalid string position");
-  v7 = *(_DWORD *)(a2 + 16) - a3;
-  if ( a4 < v7 )
-    v7 = a4;
-  if ( this == a2 )
+  else
   {
-    if ( *(_DWORD *)(this + 16) < v7 + a3 )
-      sub_4FC45C("invalid string position");
-    sub_406580(v7 + a3);
-    sub_405F20(0, a3);
+    _lock(((signed int)(a1 - (_DWORD)&off_6E1AE0) >> 5) + 16);
+    *(_DWORD *)(a1 + 12) |= 0x8000u;
   }
-  else if ( (unsigned __int8)sub_4064B0(v7, 0) )
-  {
-    if ( *(_DWORD *)(a2 + 20) < 0x10u )
-      v5 = a2;
-    else
-      v5 = *(_DWORD *)a2;
-    if ( *(_DWORD *)(v8 + 20) < 0x10u )
-      v6 = (void *)v8;
-    else
-      v6 = *(void **)v8;
-    if ( v7 )
-      memmove_0(v6, (const void *)(a3 + v5), v7);
-    sub_406580(v7);
-  }
-  return v8;
 }
