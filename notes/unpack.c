98043aee7eb9a77d2319e98377bf1efb1e523791
Five <i.f-ve@ya.ru>
2017-02-20 19:34:05 -0500

    Update and rename unpack.txt to unpack.c

---
+++ notes/unpack.c
@@ -0,0 +1,36 @@
+/** 
+ *   This file is part of rebecca.
+ *
+ *   rebecca is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   rebecca is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with rebecca.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+A good way to check unpack of blackcipher is to do run bat with. Works with all blackcipher apps but not game.
+
+start blackcipher.aes /showinstance
+
+other commands are here: 
+https://i.imgur.com/6cZhVMa.png
+
+
+
+if (it shows then)
+  winlic is there
+else if (error)
+  you goofed the unpack
+else 
+   nothing then no winlic
+
+finally
+  you can unpack the huge as fuck vm junk code
+*/
