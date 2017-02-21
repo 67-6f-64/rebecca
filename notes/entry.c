9d618128d4f36d9122bdd8b109f803059ca2f042
Five <i.f-ve@ya.ru>
2017-02-20 19:32:43 -0500

    Update and rename entry.txt to entry.c

---
+++ notes/entry.c
@@ -0,0 +1,22 @@
+/** 
+ *   This file is part of rebecca.
+ *
+ *   rebecca is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   rebecca is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with rebecca.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+oep
+5A5C8C
+
+oep jmp to winmain
+5A5B15 
